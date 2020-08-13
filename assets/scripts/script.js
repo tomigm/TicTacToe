eac7bb591afca40cbbae411e987f41167df0a37f
tomigm <tgmoron@gmail.com>
2020-08-13 19:50:51 -0300

    Array items linked to DOM with data-index

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@
 // &#9711 : Large circle
 // &times : Cross
 
-
+//CREATES BOARD
 const Gameboard = (() => {
     
     
@@ -15,22 +15,26 @@ const Gameboard = (() => {
     ];
     let board = document.getElementById("board");
 
-    let createRow = (row) => {
+    let createRow = (row, index) => {
         let rowR = document.createElement("div");
         rowR.classList.add("row");
+        
+        rowR.dataset.index = index;
         board.appendChild(rowR);
 
-        row.forEach(box => {
+        row.forEach(function (box, index) {
             let boxR = document.createElement("div");
             boxR.classList.add("box");
             boxR.innerHTML = box;
+            
+            boxR.dataset.index = index;
             rowR.appendChild(boxR);          
             
         })
     }
 
     let render = () => {
-        gameboard.forEach(row => createRow(row));
+        gameboard.forEach(createRow);
 
 
     }
@@ -41,14 +45,19 @@ const Gameboard = (() => {
 
 })();
 
-
+// CREATES NEW PLAYER
 const Player = (name) => {
 
-    
+    const getName = () => name;
 
     const play = function () {
 
     }
+}
+
+//CREATES NEW GAME
+const Game = () => {
+
 }
 
 Gameboard.render();
