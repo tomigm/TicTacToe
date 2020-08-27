632ababa4a1d10a9827311c4c9415e1fee78c9be
tomigm <tgmoron@gmail.com>
2020-08-27 15:52:14 -0300

    code cleaned

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,5 @@
 
-// &#9675; : Cricle
-// &#9711 : Large circle
-// &times : Cross
-
-/* DUMMY
-let gameboard = [
-       ["&#9675", "&times", "&times"], // row
-       ["&times", "&times", "&times"],
-       ["&times", "&times", "&times"]
-    ];
-*/
+// &#9675; : Cricle    &#9711 : Large circle    &times : Cross
 
 //CREATES BOARD
 const Gameboard = (() => {
@@ -19,31 +9,23 @@ const Gameboard = (() => {
        ["", "", ""],
        ["", "", ""],       
     ];
-
-    const resetArray = function() {
-        
+    const resetArray = function() {        
         for (let row = 0; row < board.length; row++) {          
             for (let box = 0; box < board[row].length; box++) {
                 board[row][box] = "";
             }
         }
-
     }   
-
     let boardDOM = document.getElementById("board");
-
     let createRow = (row, index) => {
         let rowR = document.createElement("div");
-        rowR.classList.add("row");
-        
+        rowR.classList.add("row");        
         rowR.dataset.index = index;
         boardDOM.appendChild(rowR);
-
         row.forEach(function (box, index) {
             let boxR = document.createElement("div");
             boxR.classList.add("box");
-            boxR.innerHTML = box;
-            
+            boxR.innerHTML = box;           
             boxR.dataset.index = index;
             rowR.appendChild(boxR);          
             
@@ -53,28 +35,20 @@ const Gameboard = (() => {
     let resetBoard = () => {
         // check if booklist has childs
          //if yes > while booklist have child, remove child
-       if(boardDOM.hasChildNodes()) {
-         
+       if(boardDOM.hasChildNodes()) {       
          while (boardDOM.hasChildNodes()) {
            boardDOM.removeChild(boardDOM.lastChild);
          }
          return
        }
        else {return}
-       
        }
-
     let render = () => {
         resetBoard();
         board.forEach(createRow);
         const boxes = document.querySelectorAll('.box');
         boxes.forEach(div => div.addEventListener('click', gameflow.playerTurn))
-
-
-
     }
-
-    
     return {
         board,
         render,
@@ -85,7 +59,6 @@ const Gameboard = (() => {
 
 // CREATES NEW PLAYER
 const Player = (name, sign) => {
-
     const getName = () => name;
     const getSign = () => sign;
     const points = 0;
@@ -100,11 +73,9 @@ const Player = (name, sign) => {
         console.log (boxIndex);
         Gameboard.board[rowIndex][boxIndex] = getSign();
         Gameboard.render();
-        gameflow.gameCheck();
-        
+        gameflow.gameCheck(); 
 
     }
-
     const resetValues = function(reset) {
         moves = reset;
         
@@ -127,7 +98,6 @@ const Gameflow = () => {
         HtmlFunctionality.p2Points.textContent = '0';
         HtmlFunctionality.displayName.textContent = ':)'
         Gameboard.render();
-
     }
     
     const restartGame = function () {
@@ -135,18 +105,16 @@ const Gameflow = () => {
         playerOne.resetValues(0)
         playerTwo.resetValues(0)
         Gameboard.resetArray();
-        return Gameboard.render();     
-
+        return Gameboard.render();
     }
+
     const roundWinner = function () {
         let totalPlays = playerOne.getMoves() + playerTwo.getMoves();
-            if (winSign === '&times') {
-               
+            if (winSign === '&times') {               
                 playerOne.points += 1;
                 HtmlFunctionality.displayName.textContent = `${playerOne.getName()} wins` // TODO DOM
                 HtmlFunctionality.p1Points.textContent = playerOne.points;
-                return restartGame();
-                
+                return restartGame();                
             }
             else if (winSign === '&#9675') {
                 playerTwo.points += 1;
@@ -160,10 +128,9 @@ const Gameflow = () => {
             }
             else {console.log('playing...')}
         }
+
     const gameCheck = function (){
-            
-
-
+    
         let arr = Gameboard.board
         // Check for horizontal win 
         for (let i = 0; i < arr.length; i++) {
@@ -174,7 +141,6 @@ const Gameflow = () => {
                     }break;           
             }
         }
-
         // Check for vertical win 
         for (let i = 0; i < arr.length; i++){
                 if (arr[0][i] !== "" && arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i] ){
@@ -184,54 +150,42 @@ const Gameflow = () => {
                     }break;                   
             }
         }
-
         // Check for diagonal win (upper left to bottom right) || Check for diagonal win (upper right to bottom left)
         if ((arr[0][0] !== "" && arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) || (arr[0][0] !== "" && arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0])){
             if (winSign == ""){
                 winSign = arr[1][1];
                 return roundWinner(); 
-            }
-            
-        }
-            
+            }            
+        }            
         return roundWinner();
     }
 
     const playerTurn = event => {
         
-        if (playerOne.getMoves() == playerTwo.getMoves()){
-            
-            playerOne.play(event);
-            
+        if (playerOne.getMoves() == playerTwo.getMoves()){            
+            playerOne.play(event);            
         }
-
         else if (playerOne.getMoves() > playerTwo.getMoves()){
-
             playerTwo.play(event);
         }
         else {return}
-
     }
-
         return {playerTurn, gameCheck, newGame};   
 };
 
 let playerOne, playerTwo;
 const gameflow = Gameflow();
 
+//* THIS BLOCK SETS ALL HTML INTERACTION & DOM MANIPULATION 
 const HtmlFunctionality = (() => {
     const display = document.getElementById("display");
     let displayName = document.getElementById('displayName'); 
     let p1Points = document.getElementById('p1Points');
     let p2Points = document.getElementById('p2Points'); 
-
-    let P1name = document.getElementById('playerOne')
-    let P2name = document.getElementById('playerTwo')
-
+    let P1name = document.getElementById('playerOne');
+    let P2name = document.getElementById('playerTwo');
     const play = document.getElementById('playBtn');
-play.addEventListener('click', gameflow.newGame)
-    
-
+play.addEventListener('click', gameflow.newGame);
 
     // Get the modal
     const modal = document.getElementById("modal");    
@@ -247,16 +201,11 @@ play.addEventListener('click', gameflow.newGame)
     closeModal.onclick = function() {
       modal.style.display = "none";
     }
-
     play.onclick = function () {
         modal.style.display = "none"
     }
-
     return {display, displayName, p1Points, p2Points, P1name, P2name}
 
 })()
 
 HtmlFunctionality;
-
-
-
